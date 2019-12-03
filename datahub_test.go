6ced87645148d7c438cdfdfef8887587086b63d4
Arief Darmawan <arief@eaciit.com>
2019-12-04 00:45:56 +0700

    init

---
+++ datahub_test.go
@@ -0,0 +1,280 @@
+package datahub_test
+
+import (
+	"fmt"
+	"testing"
+
+	"git.eaciitapp.com/sebar/dbflex/orm"
+
+	"git.eaciitapp.com/sebar/dbflex"
+	"github.com/ariefdarmawan/datahub"
+
+	_ "github.com/eaciit/flexmgo"
+	"github.com/smartystreets/goconvey/convey"
+	cv "github.com/smartystreets/goconvey/convey"
+)
+
+var (
+	connTxt = "mongodb://localhost:27017/testdb"
+)
+
+func getConn() (dbflex.IConnection, error) {
+	conn, err := dbflex.NewConnectionFromURI(connTxt, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = conn.Connect(); err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func TestHubNoPool(t *testing.T) {
+	convey.Convey("prepare connection", t, func() {
+		conn1, err := getConn()
+		convey.So(err, convey.ShouldBeNil)
+		defer conn1.Close()
+
+		conn1.Execute(dbflex.From(NewDummy(1).TableName()).Delete(), nil)
+		convey.Convey("prepare hub and generate data", func() {
+			hub := datahub.NewHub(getConn, false, 0)
+			defer hub.Close()
+
+			var err error
+			i := 0
+			for {
+				i++
+				if i > 50 {
+					break
+				}
+
+				d := NewDummy(i)
+				err = hub.Insert(d)
+				if err != nil {
+					break
+				}
+			}
+
+			convey.So(err, convey.ShouldBeNil)
+			cursor := conn1.Cursor(dbflex.From(NewDummy(90).TableName()).Select(), nil)
+			convey.So(cursor.Error(), convey.ShouldBeNil)
+			defer cursor.Close()
+			var res1, res2 []*Dummy
+			err = cursor.Fetchs(&res1, 0)
+			convey.So(err, convey.ShouldBeNil)
+			convey.So(len(res1), convey.ShouldEqual, 50)
+
+			convey.Convey("gets and filter", func() {
+				err = hub.Gets(NewDummy(1),
+					dbflex.NewQueryParam().SetWhere(dbflex.And(dbflex.Gte("ref1", 10), dbflex.Lte("ref1", 15))),
+					&res2)
+				cv.So(err, cv.ShouldBeNil)
+				cv.So(len(res2), cv.ShouldEqual, 6)
+
+				convey.Convey("update", func() {
+					for _, r := range res2 {
+						if r.Ref1 > 12 && r.Ref1 <= 14 {
+							r.Ref2 = 100
+						}
+						err = hub.Update(r)
+						if err != nil {
+							break
+						}
+					}
+
+					cv.So(err, cv.ShouldBeNil)
+					hub.Gets(NewDummy(1), dbflex.NewQueryParam().SetWhere(dbflex.Eq("ref2", 100)), &res2)
+					cv.So(len(res2), cv.ShouldEqual, 2)
+
+					convey.Convey("save", func() {
+						//-- update ref2 to 200 for 13 and 14
+						for _, r := range res2 {
+							r.Ref2 = 200
+							hub.Save(r)
+						}
+
+						//-- insert ref2 200 for 100 - 105
+						for i := 100; i <= 105; i++ {
+							r := NewDummy(i)
+							r.Ref2 = 200
+							err = hub.Save(r)
+							if err != nil {
+								break
+							}
+						}
+
+						cv.So(err, cv.ShouldBeNil)
+						hub.Gets(NewDummy(1), dbflex.NewQueryParam().SetWhere(dbflex.Eq("ref2", 200)), &res2)
+						cv.So(len(res2), cv.ShouldEqual, 8)
+
+						convey.Convey("delete", func() {
+							//-- delete 30 - 39
+							for i := 30; i <= 39; i++ {
+								d := NewDummy(i)
+								if err = hub.Delete(d); err != nil {
+									break
+								}
+							}
+
+							hub.Gets(NewDummy(1), nil, &res2)
+							cv.So(len(res2), cv.ShouldEqual, 46)
+
+							convey.Convey("get", func() {
+								d1 := NewDummy(20)
+								d2 := NewDummy(20)
+
+								d1.Ref2 = 85
+								hub.Save(d1)
+
+								err = hub.Get(d2)
+								cv.So(err, cv.ShouldBeNil)
+								cv.So(d1.Ref2, cv.ShouldEqual, d2.Ref2)
+							})
+						})
+					})
+				})
+			})
+		})
+	})
+}
+
+func TestHubWithPool(t *testing.T) {
+	convey.Convey("prepare connection", t, func() {
+		conn1, err := getConn()
+		convey.So(err, convey.ShouldBeNil)
+		defer conn1.Close()
+
+		conn1.Execute(dbflex.From(NewDummy(1).TableName()).Delete(), nil)
+		convey.Convey("prepare hub and generate data", func() {
+			hub := datahub.NewHub(getConn, true, 10)
+			defer hub.Close()
+
+			var err error
+			i := 0
+			for {
+				i++
+				if i > 50 {
+					break
+				}
+
+				d := NewDummy(i)
+				err = hub.Insert(d)
+				if err != nil {
+					break
+				}
+			}
+
+			convey.So(err, convey.ShouldBeNil)
+			cursor := conn1.Cursor(dbflex.From(NewDummy(90).TableName()).Select(), nil)
+			convey.So(cursor.Error(), convey.ShouldBeNil)
+			defer cursor.Close()
+			var res1, res2 []*Dummy
+			err = cursor.Fetchs(&res1, 0)
+			convey.So(err, convey.ShouldBeNil)
+			convey.So(len(res1), convey.ShouldEqual, 50)
+
+			convey.Convey("gets and filter", func() {
+				err = hub.Gets(NewDummy(1),
+					dbflex.NewQueryParam().SetWhere(dbflex.And(dbflex.Gte("ref1", 10), dbflex.Lte("ref1", 15))),
+					&res2)
+				cv.So(err, cv.ShouldBeNil)
+				cv.So(len(res2), cv.ShouldEqual, 6)
+
+				convey.Convey("update", func() {
+					for _, r := range res2 {
+						if r.Ref1 > 12 && r.Ref1 <= 14 {
+							r.Ref2 = 100
+						}
+						err = hub.Update(r)
+						if err != nil {
+							break
+						}
+					}
+
+					cv.So(err, cv.ShouldBeNil)
+					hub.Gets(NewDummy(1), dbflex.NewQueryParam().SetWhere(dbflex.Eq("ref2", 100)), &res2)
+					cv.So(len(res2), cv.ShouldEqual, 2)
+
+					convey.Convey("save", func() {
+						//-- update ref2 to 200 for 13 and 14
+						for _, r := range res2 {
+							r.Ref2 = 200
+							hub.Save(r)
+						}
+
+						//-- insert ref2 200 for 100 - 105
+						for i := 100; i <= 105; i++ {
+							r := NewDummy(i)
+							r.Ref2 = 200
+							err = hub.Save(r)
+							if err != nil {
+								break
+							}
+						}
+
+						cv.So(err, cv.ShouldBeNil)
+						hub.Gets(NewDummy(1), dbflex.NewQueryParam().SetWhere(dbflex.Eq("ref2", 200)), &res2)
+						cv.So(len(res2), cv.ShouldEqual, 8)
+
+						convey.Convey("delete", func() {
+							//-- delete 30 - 39
+							for i := 30; i <= 39; i++ {
+								d := NewDummy(i)
+								if err = hub.Delete(d); err != nil {
+									break
+								}
+							}
+
+							hub.Gets(NewDummy(1), nil, &res2)
+							cv.So(len(res2), cv.ShouldEqual, 46)
+
+							convey.Convey("get", func() {
+								d1 := NewDummy(20)
+								d2 := NewDummy(20)
+
+								d1.Ref2 = 85
+								hub.Save(d1)
+
+								err = hub.Get(d2)
+								cv.So(err, cv.ShouldBeNil)
+								cv.So(d1.Ref2, cv.ShouldEqual, d2.Ref2)
+							})
+						})
+					})
+				})
+			})
+		})
+	})
+}
+
+func NewDummy(i int) *Dummy {
+	d := new(Dummy)
+	d.ID = fmt.Sprintf("User-%d", i)
+	d.Name = fmt.Sprintf("Employee %d", i)
+	d.Ref1 = i
+	d.Ref2 = 0
+	return d
+}
+
+type Dummy struct {
+	orm.DataModelBase `bson:"-" json:"-" ecname:"-"`
+
+	ID   string `bson:"_id" json:"_id" ecname:"_id"`
+	Name string
+	Ref1 int
+	Ref2 int
+}
+
+func (d *Dummy) TableName() string {
+	return "dummyTable"
+}
+
+func (d *Dummy) GetID() ([]string, []interface{}) {
+	return []string{"_id"}, []interface{}{d.ID}
+}
+
+func (d *Dummy) SetID(key ...interface{}) {
+	d.ID = key[0].(string)
+}
---
+++ hub.go
@@ -0,0 +1,227 @@
+package datahub
+
+import (
+	"fmt"
+	"sync"
+
+	"git.eaciitapp.com/sebar/dbflex"
+	"git.eaciitapp.com/sebar/dbflex/orm"
+)
+
+type Hub struct {
+	connFn   func() (dbflex.IConnection, error)
+	usePool  bool
+	pool     *dbflex.DbPooling
+	poolSize int
+
+	poolItems []*dbflex.PoolItem
+	mtx       *sync.Mutex
+}
+
+func NewHub(fn func() (dbflex.IConnection, error), usePool bool, poolsize int) *Hub {
+	h := new(Hub)
+	h.connFn = fn
+	h.usePool = usePool
+	h.poolSize = poolsize
+	return h
+}
+
+func (h *Hub) getConnFromPool() (int, dbflex.IConnection, error) {
+	if h.poolSize == 0 {
+		h.poolSize = 100
+	}
+
+	if h.mtx == nil {
+		h.mtx = new(sync.Mutex)
+	}
+
+	if h.pool == nil {
+		h.pool = dbflex.NewDbPooling(h.poolSize, h.connFn)
+	}
+
+	it, err := h.pool.Get()
+	if err != nil {
+		return -1, nil, fmt.Errorf("unable get connection from pool. %s", err.Error())
+	}
+
+	conn := it.Connection()
+	idx := -1
+	h.mtx.Lock()
+	defer h.mtx.Unlock()
+
+	h.poolItems = append(h.poolItems, it)
+	idx = len(h.poolItems) - 1
+	return idx, conn, nil
+}
+
+func (h *Hub) closeConn(idx int, conn dbflex.IConnection) {
+	if !h.usePool {
+		conn.Close()
+	}
+
+	if idx < len(h.poolItems) && idx != -1 {
+		if h.mtx == nil {
+			h.mtx = new(sync.Mutex)
+		}
+
+		h.mtx.Lock()
+		defer h.mtx.Unlock()
+
+		itemCount := len(h.poolItems)
+		h.poolItems[idx].Release()
+
+		if itemCount == 0 {
+			h.poolItems = []*dbflex.PoolItem{}
+		} else if idx == 0 {
+			h.poolItems = h.poolItems[1:]
+		} else if idx == len(h.poolItems)-1 {
+			h.poolItems = h.poolItems[:idx]
+		} else {
+			h.poolItems = append(h.poolItems[:idx], h.poolItems[idx+1:]...)
+		}
+	}
+}
+
+func (h *Hub) getConn() (int, dbflex.IConnection, error) {
+	if h.connFn == nil {
+		return -1, nil, fmt.Errorf("connection fn is not yet defined")
+	}
+
+	if h.usePool {
+		return h.getConnFromPool()
+	}
+
+	conn, err := h.connFn()
+	if err != nil {
+		return -1, nil, fmt.Errorf("unable to open connection. %s", err.Error())
+	}
+	return -1, conn, nil
+}
+
+func (h *Hub) UsePool() bool {
+	return h.usePool
+}
+
+func (h *Hub) PoolSize() int {
+	return h.poolSize
+}
+
+func (h *Hub) Save(data orm.DataModel) error {
+	idx, conn, err := h.getConn()
+	if err != nil {
+		return fmt.Errorf("connection error. %s", err.Error())
+	}
+	defer h.closeConn(idx, conn)
+
+	if err = orm.Save(conn, data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *Hub) Insert(data orm.DataModel) error {
+	idx, conn, err := h.getConn()
+	if err != nil {
+		return fmt.Errorf("connection error. %s", err.Error())
+	}
+	defer h.closeConn(idx, conn)
+
+	if err = orm.Insert(conn, data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *Hub) Update(data orm.DataModel) error {
+	idx, conn, err := h.getConn()
+	if err != nil {
+		return fmt.Errorf("connection error. %s", err.Error())
+	}
+	defer h.closeConn(idx, conn)
+
+	if err = orm.Update(conn, data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *Hub) Delete(data orm.DataModel) error {
+	idx, conn, err := h.getConn()
+	if err != nil {
+		return fmt.Errorf("connection error. %s", err.Error())
+	}
+	defer h.closeConn(idx, conn)
+
+	if err = orm.Delete(conn, data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/*
+func (h *Hub) Cursor(data orm.DataModel, parm dbflex.QueryParam) (dbflex.ICursor, error) {
+	idx, conn, err := h.getConn()
+	if err != nil {
+		return nil, fmt.Errorf("connection error. %s", err.Error())
+	}
+	defer h.closeConn(idx, conn)
+
+	qry := dbflex.From(data.TableName())
+	if w := parm.Where; w != nil {
+		qry.Where(w)
+	}
+	if o := parm.Sort; len(o) > 0 {
+		qry.OrderBy(o...)
+	}
+	if o := parm.Skip; o > 0 {
+		qry.Skip(o)
+	}
+	if o := parm.Take; o > 0 {
+		qry.Take(o)
+	}
+	cur := conn.Cursor(qry, nil)
+	if cur.Error() != nil {
+		cur.Close()
+		return nil, fmt.Errorf("cursor error. %s", cur.Error().Error())
+	}
+	return cur, nil
+}
+*/
+
+func (h *Hub) Get(data orm.DataModel) error {
+	idx, conn, err := h.getConn()
+	if err != nil {
+		return fmt.Errorf("connection error. %s", err.Error())
+	}
+	defer h.closeConn(idx, conn)
+
+	if err = orm.Get(conn, data); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *Hub) Gets(data orm.DataModel, parm *dbflex.QueryParam, dest interface{}) error {
+	idx, conn, err := h.getConn()
+	if err != nil {
+		return fmt.Errorf("connection error. %s", err.Error())
+	}
+	defer h.closeConn(idx, conn)
+
+	if err = orm.Gets(conn, data, dest, parm); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *Hub) Close() {
+	if h.usePool {
+		h.pool.Close()
+	}
+}
